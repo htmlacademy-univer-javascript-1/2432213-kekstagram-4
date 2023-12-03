7e68d3a6be93da2ee4d8c2f95299963038fda391
Александр <belalex.tab@gmail.com>
2023-12-03 21:32:36 +0500

    end commit

---
+++ js/function.js
@@ -0,0 +1,25 @@
+=======
+function isMeetingWithinWorkingHours(startOfWork, endOfWork, startOfMeeting, meetingDuration) {
+  function timeToMinutes(time) {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  const startOfWorkMinutes = timeToMinutes(startOfWork);
+  const endOfWorkMinutes = timeToMinutes(endOfWork);
+  const startOfMeetingMinutes = timeToMinutes(startOfMeeting);
+
+  const endOfMeetingMinutes = startOfMeetingMinutes + meetingDuration;
+
+  if (startOfMeetingMinutes >= startOfWorkMinutes && endOfMeetingMinutes <= endOfWorkMinutes) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isMeetingWithinWorkingHours('08:00', '17:30', '14:00', 90);
+isMeetingWithinWorkingHours('8:0', '10:0', '8:0', 120);
+isMeetingWithinWorkingHours('08:00', '14:30', '14:00', 90);
+isMeetingWithinWorkingHours('14:00', '17:30', '08:0', 90);
+isMeetingWithinWorkingHours('8:00', '17:30', '08:00', 900);
